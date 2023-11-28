c428ff49da762541700eb118a4b8763082c88ec4
Ichsan Ashiddiqi <42838497+ichsanashiddiqi@users.noreply.github.com>
2023-11-28 15:35:20 +0700

    Update App.php

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      *    http://example.com/
      */
-    public string $baseURL = 'http://localhost:8080/';
+    public string $baseURL = 'https://sims-ppob-ichsan-ashiddiqi-production.up.railway.app/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
